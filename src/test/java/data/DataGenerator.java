6f98bf36617c156154e2b7b31871a6435a7a5a40
Iulia <ul4ik17@gmail.com>
2024-10-13 22:04:05 +0300

    update files

--- src/test/java/data/DataGenerator.java
+++ src/test/java/data/DataGenerator.java
@@ -1,29 +1,43 @@
+
 package data;
 
 import com.github.javafaker.Faker;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 
 public class DataGenerator {
     private DataGenerator() {
     }
 
-    public static class Registration {
+    public static String generateDate(int days) {
+        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
 
-        private Registration() {
-        }
+    public static String generateCity() {
+        List<String> list = Arrays.asList("Архангельск", "Астрахань", "Барнаул", "Владикавказ",
+                "Горно-Алтайск", "Йошкар-Ола", "Казань", "Калининград", "Калуга", "Краснодар", "Махачкала",
+                "Нарьян-Мар", "Салехард", "Самара", "Саранск", "Саратов", "Хабаровск", "Ханты-Мансийск", "Южно-Сахалинск",
+                "Великий Новгород", "Владивосток", "Владимир", "Вологда", "Рязань", "Биробиджан", "Чебоксары", "Москва",
+                "Санкт-Петербург", "Ульяновск", "Симферополь", "Ростов-на-Дону");
+        Random rand = new Random();
+        String city = list.get(rand.nextInt(list.size()));
+        return city;
+    }
 
-        public static RegByCardInfo generateByCard(String locale, int days, int changeDays) {
-            Faker faker = new Faker(new Locale(locale));
-            return new RegByCardInfo(
-                    faker.address().city(),
-                    LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
-                    LocalDate.now().plusDays(changeDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
-                    faker.name().lastName() + " " + faker.name().firstName(),
-                    faker.phoneNumber().phoneNumber()
-            );
-        }
+    public static String generateName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().firstName() + " " + faker.name().lastName();
+    }
+
+    public static String generatePhone() {
+
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
     }
 }
--- src/test/java/test/CardOrderTest.java
+++ src/test/java/test/CardOrderTest.java
@@ -1,166 +1,48 @@
 package test;
 
-import com.codeborne.selenide.logevents.SelenideLogger;
-import data.DataGenerator;
-import data.RegByCardInfo;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import data.DataGenerator;
+
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
-import static java.time.Duration.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-public class CardOrderTest {
+class CardOrderTest {
 
-    RegByCardInfo info = DataGenerator.Registration.generateByCard("ru", 3, 5);
+    String name = DataGenerator.generateName();
+    String phone = DataGenerator.generatePhone();
+    String city = DataGenerator.generateCity();
 
-    @BeforeAll
-    static void addListener(){
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
-    }
-
-    @AfterAll
-    static void removeListener(){
-        SelenideLogger.removeListener("AllureSelenide");
+    @BeforeEach
+    public void setUp() {
+        Configuration.browser = "chrome";
+        open("http://localhost:9999/");
     }
 
     @Test
-    void shouldReturnSuccess() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue(info.getCity());
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(String.valueOf(info.getDate()));
-        $("[data-test-id='name'] input").setValue(info.getName());
-        $("[data-test-id='phone'] input").setValue(info.getPhone());
-        $("[data-test-id='agreement'] .checkbox__box").click();
-        $(".button").click();
-        $(".notification__content").shouldBe(visible, ofSeconds(15)).shouldHave(exactText("Встреча успешно запланирована на " + info.getDate()));
-    }
-
-    @Test
-    void shouldRescheduleSuccess() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue(info.getCity());
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(String.valueOf(info.getDate()));
-        $("[data-test-id='name'] input").setValue(info.getName());
-        $("[data-test-id='phone'] input").setValue(info.getPhone());
-        $("[data-test-id='agreement'] .checkbox__box").click();
-        $(".grid-row button").click();
-        $(".notification__content").shouldBe(visible, ofSeconds(15)).shouldHave(exactText("Встреча успешно запланирована на " + info.getDate()));
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(String.valueOf(info.getChangeDate()));
-        $(".grid-row button").click();
-        $("[data-test-id='replan-notification'] .button__text").shouldBe(visible, ofSeconds(15)).click();
-        $(".notification__content").shouldBe(visible, ofSeconds(15)).shouldHave(exactText("Встреча успешно запланирована на " + info.getChangeDate()));
-    }
-
-    @Test
-    void shouldReturnFailWithEmptyCity() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("");
-        $(".grid-row button").click();
-        $("[data-test-id='city'] .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void shouldReturnFailWithIncorrectCity() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Moscow");
-        $(".grid-row button").click();
-        $("[data-test-id='city'] .input__sub").shouldHave(text("Доставка в выбранный город недоступна"));
-    }
-
-    @Test
-    void shouldReturnFailWithEmptyDate() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $(".grid-row button").click();
-        $("[data-test-id='date'] .input__sub").shouldHave(text("Неверно введена дата"));
-    }
-
-    @Test
-    void shouldReturnFailWithIncorrectDate() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        $(".grid-row button").click();
-        $("[data-test-id='date'] .input__sub").shouldHave(text("Заказ на выбранную дату невозможен"));
-    }
-
-    @Test
-    void shouldReturnFailWithEmptyNames() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(info.getDate());
-        $("[data-test-id='name'] input").setValue("");
-        $(".grid-row button").click();
-        $("[data-test-id='name'] .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void shouldReturnFailWithIncorrectNames() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(info.getDate());
-        $("[data-test-id='name'] input").setValue("Ivan");
-        $(".grid-row button").click();
-        $("[data-test-id='name'] .input__sub").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-    }
-
-    @Test
-    void shouldReturnFailWithEmptyPhone() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(info.getDate());
-        $("[data-test-id='name'] input").setValue("Иван Иванов");
-        $("[data-test-id='phone'] input").setValue("");
-        $(".grid-row button").click();
-        $("[data-test-id='phone'] .input__sub").shouldHave(text("Поле обязательно для заполнения"));
-    }
-
-    @Test
-    void shouldReturnFailWithIncorrectPhone() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(info.getDate());
-        $("[data-test-id='name'] input").setValue("Иван Иванов");
-        $("[data-test-id='phone'] input").setValue("8953");
-        $(".grid-row button").click();
-        $("[data-test-id='phone'] .input__sub").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
-
-    @Test
-    void shouldReturnFailWithEmptyCheckbox() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
-        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(info.getDate());
-        $("[data-test-id='name'] input").setValue("Иван Иванов");
-        $("[data-test-id='phone'] input").setValue("+79003332211");
-        $(".grid-row button").click();
-        $("[data-test-id='agreement'] .checkbox__text").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    void shouldSubmitRequest() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue(DataGenerator.generateCity());
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(3));
+        form.$("[data-test-id=name] input").setValue(DataGenerator.generateName());
+        form.$("[data-test-id=phone] input").setValue(DataGenerator.generatePhone());
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".notification_status_ok").shouldBe(visible);
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(3)));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
+        form.$(".button").click();
+        $("[data-test-id=replan-notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $(withText("Перепланировать")).click();
+        $(".notification_status_ok").shouldBe(visible);
+        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(7)));
     }
 }
